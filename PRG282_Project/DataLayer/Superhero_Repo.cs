3170a24082fa10db99de3f6bb3418d30c898f4ec
FreerkvdB <vdbosfreerk@gmail.com>
2025-10-10 22:29:59 +0200

    Write to file method
    I added the function to save the data to the superhero.txt file and the delete superhero method.

--- PRG282_Project/DataLayer/Superhero_Repo.cs
+++ PRG282_Project/DataLayer/Superhero_Repo.cs
@@ -11,41 +11,83 @@ namespace PRG282_Project.Data_Layer
 {
     internal class Superhero_Repo
     {
-        private readonly string filepath = "superhero_repo.txt"; // Create a text file to store superhero data
+        private readonly string filepath = "superhero.txt"; // Create a text file to store superhero data
 
         // We create a list to hold superhero objects
         public List<Superhero> GetAllHeroes()
         {
-            var heroes = new List<Superhero>();
-
-            if (!File.Exists(filepath))
+            try
             {
-                File.Create(filepath).Close(); // Create the file if it doesn't exist
-            }
+                var heroes = new List<Superhero>();
 
-            // Read all lines from the file and parse them into Superhero objects
-            foreach (string line in File.ReadAllLines(filepath))
-            {
-                var data = line.Split(','); // Assuming data is comma-separated
-
-                if (data.Length >= 7) // Ensure there are enough data fields
+                if (!File.Exists(filepath))
                 {
-                    var hero = new Superhero
-                    (
-                       int.Parse(data[0]),
-                       data[1],
-                       int.Parse(data[2]),
-                       data[3],
-                       int.Parse(data[4]),
-                       data[5],
-                       data[6]
-                    );
-                   heroes.Add(hero);
+                    File.Create(filepath).Close(); // Create the file if it doesn't exist
+                }
+
+                // Read all lines from the file and parse them into Superhero objects
+                foreach (string line in File.ReadAllLines(filepath))
+                {
+                    var data = line.Split(','); // Assuming data is comma-separated
+
+                    if (data.Length >= 7) // Ensure there are enough data fields
+                    {
+                        var hero = new Superhero
+                        (
+                           int.Parse(data[0]),
+                           data[1],
+                           int.Parse(data[2]),
+                           data[3],
+                           int.Parse(data[4]),
+                           data[5],
+                           data[6]
+                        );
+                        heroes.Add(hero);
+                    }
+                }
+
+                return heroes;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                return new List<Superhero>();
+            }
+        }
+
+        //Save all heroes to the file
+        public void SaveAllHeroes(List<Superhero> heroes)
+        {
+            try
+            {
+                var lines = new List<string>(); // Create a list to hold the lines to write to the file
+
+                foreach (var hero in heroes)
+                {
+                    lines = heroes.Select(h => $"{h.SuperheroID1}\t {h.SuperheroName1}\t {h.SuperheroAge1}\t {h.SuperPower1}\t {h.ExamScore1}\t {h.Rank1}\t {h.ThreadLevel1}").ToList();
+                    File.WriteAllLines(filepath, lines);
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while writing to the file: {ex.Message}");
+            }
+        }
 
-            return heroes;
-
+        // Delete a hero from file
+        public void DeleteHero(int superheroID)
+        {
+            try
+            {
+                var heroes = GetAllHeroes();
+                heroes.RemoveAll(h => h.SuperheroID1 == superheroID);
+                SaveAllHeroes(heroes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while deleting the hero: {ex.Message}");
+            }
         }
     }
 }
